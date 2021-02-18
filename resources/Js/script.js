67388368913f399da592712f94f1c35709dceebc
Michael Neftali <michael.neftali@gmail.com>
2021-02-18 13:01:33 +0100

    added sticky header without jquery

--- resources/Js/script.js
+++ resources/Js/script.js
@@ -1,16 +1,6 @@
 $(document).ready(function() {
                                                                                                                                         
 
-    // FOR STICKY NAVIGATION
-    $('.js--section-features').waypoint(function(direction) {
-        if (direction == "down") {
-                $('nav').addClass('sticky-header');
-        } else {
-            $('nav').removeClass('sticky-header');
-        } 
-    }, {
-        offset: '100px'
-    });
 
 
 
